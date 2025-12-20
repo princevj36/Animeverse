7a7d49cfe3c9190d4af2b725cc8f21fb71a25c34
princevj36 <theanimeverse.in@gmail.com>
2025-12-20 20:26:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 import Index from "./pages/Index";
 import Shop from "./pages/Shop";
 import ProductDetail from "./pages/ProductDetail";
@@ -8,24 +9,29 @@ import OrderSuccess from "./pages/OrderSuccess";
 import Wishlist from "./pages/Wishlist";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import { Toaster as SonnerToaster } from "./components/ui/sonner";
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/order-success" element={<OrderSuccess />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/categories" element={<Navigate to="/shop" replace />} />
-      <Route path="/category/:id" element={<Navigate to="/shop" replace />} />
-      <Route path="/rare" element={<Navigate to="/shop?category=rare" replace />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/categories" element={<Navigate to="/shop" replace />} />
+        <Route path="/category/:id" element={<Navigate to="/shop" replace />} />
+        <Route path="/rare" element={<Navigate to="/shop?category=rare" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+      <SonnerToaster position="top-center" richColors />
+    </>
   );
-};
+}
 
 export default App;
